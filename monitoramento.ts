8190ab5bbe57d0339a4584bd6e39e5e6a1c81e55
Aleson Batista <alesonbatista@hotmail.com>
2025-09-29 23:42:14 -0300

    continuação do código

--- monitoramento.ts
+++ monitoramento.ts
@@ -8,17 +8,20 @@ class Produto{
         private quantidade: number
     ) {}
 
+    //getter para atributos privados
     getCodigo(): string { return this.codigo;}
     getNome(): string {return this.nome;}
     getPreco(): number {return this.preco;}
     getQuantidade(): number {return this.quantidade;}
 
+    //adiciona quantidade no estoque
     adicionar(qtd: number) {
         if (qtd <= 0) {
             throw new Error('Quantidade inválida');
         } this.quantidade += qtd;
     }
 
+    //remove quantidade do estoque e verifica se é válido
     remover(qtd: number) {
         if (qtd <= 0) {
             throw new Error('Quantidade inválida');
@@ -30,5 +33,34 @@ class Produto{
 
     toString() {
         return `Código: ${this.codigo} - ${this.nome} | R$${this.preco} | Estoque: ${this.quantidade}}`
+    }
+}
+
+//Classe abstrata (não pode ser instanciada diretamente)
+//molde para Entrada e Saida
+
+abstract class Movimentacao {
+    constructor(
+        protected produto: Produto,
+        protected quantidade: number,
+        protected data: Date = new Date()
+    ) {}
+    
+    //método abstrato: cada filha (Entrada/Saida) implementa do seu jeito
+    abstract aplicar(): void
+
+    toString(): string {
+        return `Produto: ${this.produto.getNome()} | Qtd: ${this.quantidade} | Data: ${this.data.toLocaleString()}`;
+    }
+} 
+
+//classe filha para representar entrada de produtos no estoque
+class Entrada extends Movimentacao{
+    aplicar(): void {
+        this.produto.adicionar(this.quantidade) //adiciona ao estoque
+    }
+
+    toString(): string {
+        return `[Entrada] ${super.toString()}`
     }
 }
